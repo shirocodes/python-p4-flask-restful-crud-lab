cf7a0d3cfe50e375899b239f2d673ba8be102fa2
shirocodes <ishirapt@gmail.com>
2025-06-21 19:18:06 +0300

    CRUD lab complete

--- server/app.py
+++ server/app.py
@@ -46,8 +46,34 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
-
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}
+        try:
+            data = request.get_json()
+            for attr in data:
+                if attr != "id":
+                    setattr(plant, attr, data[attr])
+            db.session.commit()
+            
+            return make_response(plant.to_dict(), 200)
+        except Exception as e:
+            return {"error": str(e)}, 500
+        
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return {"error": "Plant not found"}, 404
+        
+        try:
+            db.session.delete(plant)
+            db.session.commit()
+            
+            return '', 204
+        except Exception as e:
+            return {"error": str(e)}, 500
+        
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
